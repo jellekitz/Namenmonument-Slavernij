9672f1a68c1dd10b73d5c54e796a3be5193414eb
Jellekitz <jellekitz@gmail.com>
2022-01-19 15:59:01 +0100

    added animation

---
+++ Projectiescherm/src/components/Interface.jsx
@@ -0,0 +1,22 @@
+// import React, { useRef } from "react";
+
+const Interface = () => {
+  //   const textInput = useRef(null);
+
+  //   function handleClick() {
+  //     console.log("test");
+  //   }
+
+  return (
+    <section className="interface">
+      <button className="interface__button interface__button--highlight">
+        Zoek highlight
+      </button>
+      <button className="interface__button interface__button--pv-highlight">
+        Persoonlijk verhaal highlight
+      </button>
+    </section>
+  );
+};
+
+export default Interface;
