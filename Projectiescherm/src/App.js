87459b5c94b2712d7660de104e1a4b80b839fd40
Jellekitz <jellekitz@gmail.com>
2022-01-20 15:55:52 +0100

    added highlight buttons

--- Projectiescherm/src/App.js
+++ Projectiescherm/src/App.js
@@ -1,20 +1,48 @@
 import "./styles/App.scss";
+import React, { useState } from "react";
+import Alima from "./images/Alima.svg";
 import Title from "./components/Title";
 import Content from "./components/Content";
 import Interface from "./components/Interface";
 
 function App() {
+  // Add highlighted name
+  const [name, setName] = useState(null);
+
+  const addName = () => {
+    setName(() => {
+      return <p className="randomName--highlighted">Bangu</p>;
+    });
+  };
+
+  // Add personal highlighted name
+  const [personalName, setPersonalName] = useState(null);
+
+  const addPersonalName = () => {
+    setPersonalName(() => {
+      return (
+        <>
+          <p className="randomName--highlighted-personal">Alima</p>
+          <img
+            className="randomName--highlighted-personal__image"
+            src={Alima}
+          ></img>
+        </>
+      );
+    });
+  };
+
   return (
     <div className="App">
       <header>
         <Title />
       </header>
       <main>
-        <Content />
+        <Content name={name} personalName={personalName} />
       </main>
       <div className="overlay"></div>
       <footer>
-        <Interface />
+        <Interface addName={addName} addPersonalName={addPersonalName} />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
       </footer>
     </div>
