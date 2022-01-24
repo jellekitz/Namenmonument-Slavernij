788e14529b63cd95b2a75e577740353d9bafe296
Jellekitz <jellekitz@gmail.com>
2022-01-24 17:15:43 +0100

    added facts

--- Interactiescherm/src/Components/Facts/Facts.js
+++ Interactiescherm/src/Components/Facts/Facts.js
@@ -1,11 +1,64 @@
-import React from "react";
+import * as React from "react";
+import { tns } from "../../../node_modules/tiny-slider/src/tiny-slider";
 
-function Facts() {
+const Facts = () => {
+  const { useState, useEffect } = React;
+
+  const [slider, setSlider] = useState(0);
+
+  useEffect(() => {
+    const slider = tns({
+      container: ".facts__slider",
+      items: 1,
+      autoplay: false,
+      autoplayButtonOutput: false,
+      controls: false,
+      nav: true,
+      speed: 500,
+      navPosition: "bottom",
+      navAsThumbnails: true,
+      loop: true,
+      swipeAngle: false,
+    });
+    setSlider(slider);
+  }, []);
+
   return (
-    <div>
-      <h1></h1>
+    <div className="facts">
+      <div className="facts__wrapper">
+        <ul className="facts__slider">
+          <li className="facts__slider-item">
+            <h2>Wist je dat…</h2>
+            <p>
+              Naar schatting er totaal <b>1.000.000</b> tot <b>2.000.000</b> tot
+              slaaf gemaakte mensen waren.
+            </p>
+          </li>
+          <li className="facts__slider-item">
+            <h2>Wist je dat…</h2>
+            <p>
+              Onze datasets uit <b>ouwe geschriften</b> komen die later terug
+              gevonden zijn.
+            </p>
+          </li>
+          <li className="facts__slider-item">
+            <h2>Wist je dat…</h2>
+            <p>
+              Slaven soms <b>meerdere keren</b> in hun leven <b>verkocht</b>{" "}
+              werden.
+            </p>
+          </li>
+          <li className="facts__slider-item">
+            <h2>Wist je dat…</h2>
+            <p>
+              De kans groot is dat <b>jouw stamboom</b> ook verwant is aan de
+              slavernij.
+            </p>
+          </li>
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default Facts;
