2040e2eea8cf14bf3ab30af398d4a3dd97cb1bdc
Jellekitz <jellekitz@gmail.com>
2022-01-25 18:06:01 +0100

    added some small fixes for header and image

--- Projectiescherm/src/components/Title.jsx
+++ Projectiescherm/src/components/Title.jsx
@@ -3,9 +3,7 @@ import Counter from "./Counter";
 const Title = () => {
   return (
     <>
-      <h1 className="header__title">Tot slaaf gemaakte</h1>
-      <h2 className="header__title">De namen van de</h2>
-      <h2 className="header__title">mensen</h2>
+      <h1 className="header__title">De namen</h1>
       <Counter />
     </>
   );
