90fe5095500ec0ad2efc828de3306737d71fce4c
Ravi Stil <ravi.raaf@gmail.com>
2022-01-26 13:55:22 +0100

    Changes to Story

--- Interactiescherm/src/Components/PersonalStories/Story/Story.js
+++ Interactiescherm/src/Components/PersonalStories/Story/Story.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Story.scss";
-import womenImage from "../../../Images/PersonalStories/two-women.png"; // relative path to image
+import womenImage from "../../../Images/PersonalStories/two-women.png";
+import manImage from "../../../Images/PersonalStories/man.png";
+import footerImage from "../../../Images/PersonalStories/footer-image.png";
+import "aos/dist/aos.css";
+import Aos from "aos";
 
 function Story() {
+  useEffect(() => {
+    Aos.init({});
+  });
   return (
     <div className="story">
       <section className="story__hero">
         <div>
-          <h1>Maria</h1>
-          <p>In 1628 ingeschreven in Batavia</p>
+          <h1 data-aos="fade-down" data-aos-delay="100">
+            Maria
+          </h1>
+          <p data-aos="fade-up" data-aos-delay="300">
+            In 1628 ingeschreven in Batavia
+          </p>
         </div>
       </section>
       <section className="story__section1">
-        <img src={womenImage} />
-        <div>
+        <img
+          src={womenImage}
+          data-aos="fade-right"
+          data-aos-offset="20"
+          data-aos-delay="200"
+        />
+        <div data-aos="fade-left" data-aos-offset="400" data-aos-delay="400">
           <p>
             Maria was een vrouw waar we niet veel over weten. Maar wel dat ze in
             1628 in Batavia is ingeschreven als slaaf, <br />
@@ -32,7 +48,7 @@ function Story() {
         </div>
       </section>
       <section className="story__section2">
-        <div>
+        <div data-aos="fade-right" data-aos-offset="300" data-aos-delay="500">
           <p>
             Nadat Maria Pieter van den Broecke een zoontje schonk, maakte hij
             haar vrij en huwelijkte haar uit aan de VOC-soldaat Abraham
@@ -44,6 +60,21 @@ function Story() {
             <br /> Lees zijn verhaal ook
           </a>
         </div>
+        <img
+          src={manImage}
+          data-aos="fade-left"
+          data-aos-offset="100"
+          data-aos-delay="200"
+        />
+      </section>
+      <section className="story__section3">
+        <a href="/persoonlijke-verhalen">Bekijk meer persoonlijke verhalen</a>
+        <img
+          src={footerImage}
+          data-aos="fade-up"
+          data-aos-offset="600"
+          data-aos-delay="0"
+        />
       </section>
     </div>
   );
